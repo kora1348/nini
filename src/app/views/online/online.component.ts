74a1c5523afd1228970e6bd602052539c5eb4d76
kora1348 <amelianekor@gmail.com>
2022-11-08 14:19:16 +0100

    toto

--- src/app/views/online/online.component.ts
+++ src/app/views/online/online.component.ts
@@ -1,33 +1,75 @@
-﻿import { Component } from '@angular/core';
-import { first } from 'rxjs/operators';
+﻿import { Component } from "@angular/core";
+import { first } from "rxjs/operators";
 
-import { User } from '@app/_models';
-import { AuthenticationService, UserService } from '@app/_services';
-import { Router } from '@angular/router';
+import { User } from "@app/_models";
+import { AuthenticationService, UserService } from "@app/_services";
+import { Router } from "@angular/router";
+import { Menu } from "./types/menu";
 
 @Component({
-    selector: 'app-online',
-    templateUrl: './online.component.html',
-    styleUrls: ['./online.component.css'],
+  selector: "app-online",
+  templateUrl: "./online.component.html",
+  styleUrls: ["./online.component.css"],
 })
 export class OnlineComponent {
-    loading = false;
-    users: User[];
-    user: User;
+  loading = false;
+  users: User[];
+  user: User;
 
-    constructor(private userService: UserService,  private authenticationService: AuthenticationService, private router: Router) { 
-        this.authenticationService.user.subscribe(x => this.user = x);
-    }
+  showSideBar: boolean = false;
 
-    ngOnInit() {
-        this.loading = true;
-        this.userService.getAll().pipe(first()).subscribe(users => {
-            this.loading = false;
-            this.users = users;
-        });
-    }
+  menus: Menu[] = [
+    {
+      name: "Administration",
+      iconClass: "fas fa-desktop",
+      active: false,
+      submenu: [
+        {
+          name: "Portfolio",
+          url: "/administration/portfolio",
+        },
+      ],
+    },
+  ];
 
-    logout() {
-        this.authenticationService.logout();
-    }
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) {
+    this.authenticationService.user.subscribe((x) => (this.user = x));
+  }
+
+  ngOnInit() {
+    this.loading = true;
+    this.userService
+      .getAll()
+      .pipe(first())
+      .subscribe((users) => {
+        this.loading = false;
+        this.users = users;
+      });
+  }
+
+  logout() {
+    this.authenticationService.logout();
+  }
+
+  toggle(index: number) {
+    this.menus[index].active = !this.menus[index].active;
+  }
+
+  toggleSideBar() {
+    this.showSideBar = !this.showSideBar;
+  }
+
+  selectMenu(parentMenu: { name: string }): void {
+    this.menus.forEach((menu) => {
+      if (menu.name !== parentMenu.name) {
+        menu.active = false;
+      } else {
+        menu.active = !menu.active;
+      }
+    });
+  }
 }
